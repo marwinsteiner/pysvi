7f0dae4fd0ca1023bad1504efc171af8dc70d67e
Marwin Steiner <marwin.steiner@gmail.com>
2026-02-03 22:28:07 +0000

    feat(calibration): adds the ticker parser

--- src/pysvi/calibration.py
+++ src/pysvi/calibration.py
@@ -17,3 +17,18 @@ from py_lets_be_rational.exceptions import BelowIntrinsicException
 from py_vollib.black_scholes_merton.implied_volatility import implied_volatility as bsm_iv
 
 from .models import SVI, SSVI, ESSVI
+
+
+warnings.filterwarnings("ignore")
+
+_ticker_re = re.compile(r"SPY(\d{6})([CP])(\d+)")
+
+def parse_ticker_info(path: Path):
+    """Parse SPY option ticker from filename."""
+    m = _ticker_re.match(path.stem)
+    if not m:
+        return None
+    expiry_str, cp, strike_digits = m.groups()
+    strike = int(strike_digits) / 1000.0
+    expiry_dt = pd.to_datetime("20" + expiry_str[:2] + expiry_str[2:6], format="%Y%m%d")
+    return {"expiry": expiry_str, "expiry_dt": expiry_dt, "cp": cp, "strike": strike}
